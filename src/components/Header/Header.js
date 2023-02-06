f85006672ff3e91ef49a5a57e0ec79243f31e817
aloklearning <alokk830@gmail.com>
2023-02-06 11:58:41 +0530

    Header code is done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+import {ReactComponent as HeaderLogo} from '../../assets/rocket_launch.svg';
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <HeaderLogo stroke='white' fill='white' />
+            <p className="header-text">Spaceship Finder</p>
+        </div>
+    )
+}
+
+export default Header;
