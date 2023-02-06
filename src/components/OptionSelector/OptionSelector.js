6f9d93c576ad1e357b9af97c113cd27efd9be697
aloklearning <alokk830@gmail.com>
2023-02-06 21:37:56 +0530

    color logic is ongoing

--- src/components/OptionSelector/OptionSelector.js
+++ src/components/OptionSelector/OptionSelector.js
@@ -1,10 +1,31 @@
 import './OptionSelector.css';
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const OptionSelector = ({ paramsObject, setParamsObject }) => {
+    // For the final submission to query URL while filtering
+    const colorItems = useRef([paramsObject.colors ?? '']);
 
     const onSubmit = () => {
+        // Color selection handling
+        // If the selection contains all, then only all
+        // Else, combine all of them
+        let colorParam = '';
+        if(colorItems.current.length > 0) {
+            if(colorItems.current.includes('all')){
+                colorParam = 'all';
+            }else{
+                // Stroing the value in rgba format to be able
+                // to identify the color properly
+                colorItems.current.forEach(item => {
+                    colorParam += item[0]
+                })
+            }
+        }
         
+        setParamsObject({...paramsObject, colors: colorParam})
+        
+        // Resetting for finerhandling
+        colorItems.current = [];
     }
 
     const ColorPickerItem = () => {
@@ -15,6 +36,17 @@ const OptionSelector = ({ paramsObject, setParamsObject }) => {
 
             const onChange = () => {
                 setValue(valueStatus => valueStatus = !valueStatus)
+
+                // Works a bit opposite to the click
+                if(!value) {
+                    if(!colorItems.current.includes(colorName)) {
+                        colorItems.current.push(colorName.toLowerCase());
+                    }
+                }else{
+                    colorItems.current = colorItems.current.filter((item) => item !== colorName.toLowerCase())
+                }
+
+                console.log(colorItems.current)
             }
 
             return (
