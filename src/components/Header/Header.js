c79b0ac651da0522d1d3e9310cbd10dc78c0cb44
aloklearning <alokk830@gmail.com>
2023-02-06 15:38:27 +0530

    Header Home button is added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import './Header.css';
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { ReactComponent as HeaderLogo } from '../../assets/rocket_launch.svg';
 
 const Header = () => {
+    const location = useLocation()
+
     return (
         <div className="header-container">
             <div className='left-container'>
@@ -10,9 +13,12 @@ const Header = () => {
                 <p className="header-text">Spaceship Finder</p>
             </div>
 
+            {location.pathname === '/home' && (
             <div className='right-container'>
-                <div className='btn'>Home</div>
-            </div>
+                <Link to='/' className='btn header-btn'>
+                    Home
+                </Link>
+            </div>)}
         </div>
     )
 }
