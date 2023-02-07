b1c8afeb7b748ecc2e810825e86b256e142316bd
aloklearning <alokk830@gmail.com>
2023-02-07 13:42:04 +0530

    Speed input is added

--- src/components/OptionSelector/SpeedContainer.js
+++ src/components/OptionSelector/SpeedContainer.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 const SpeedContainer = () => {
+    const [speed, setSpeed] = useState();
+
     return (
         <div style={{marginTop: 20}}>
             <p className='header'>2. Choose the maxium speed from the below options:</p>
@@ -7,6 +11,17 @@ const SpeedContainer = () => {
             Once that is done. Check on 50 and 200 range for more than and smaller than respectively
             Then, in the query string it should go as: greater than 50 => 51-200
             smaller than 200 => 50-199, exact 50 => 50 */}
+            {/* Content Container */}
+            <div className="speed-container">
+                <input 
+                    value={speed}
+                    type='text' 
+                    maxLength={3}
+                    inputMode='numeric'
+                    placeholder='Add Maximum Speed'
+                    onChange={(e) => setSpeed(e.target.value)}
+                />
+            </div>
         </div>
     )
 }
