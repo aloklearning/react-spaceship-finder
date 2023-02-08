dc6b8bcce2ba5d254cfef2dcd173e3a43229ae77
aloklearning <alokk830@gmail.com>
2023-02-08 14:21:45 +0530

    Spaceship data moved to the json format now

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,29 +9,7 @@ const Home = () => {
     const [urlQueries, setURLQueries] = useSearchParams();
     const params = Object.fromEntries([...urlQueries]);
     
-    const spaceshipInfo = [
-        {
-            id: 1,
-            color: 'red',
-            maximum_speed: 200,
-            has_pulse_laser: true,
-            date_of_manufacture: '2020/02/13'
-        },
-        {
-            id: 2,
-            color: 'blue',
-            maximum_speed: 150,
-            has_pulse_laser: true,
-            date_of_manufacture: '2019/02/13'
-        },
-        {
-            id: 3,
-            color: 'green',
-            maximum_speed: 100,
-            has_pulse_laser: true,
-            date_of_manufacture: '2018/02/13'
-        }
-    ]
+    const spaceshipInfo = require('../../assets/spaceships.json');
 
     const EmptyContainer = () => {
         return(
